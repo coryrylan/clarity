33a0dc2ab644026edafe791ac77f6e5414c43113
Cory Rylan <crylan@vmware.com>
2019-11-19 10:30:29 -0600

    fix: stable build
    Signed-off-by: Cory Rylan <crylan@vmware.com>

--- src/dev-core/src/app/modal/modal.demo.ts
+++ src/dev-core/src/app/modal/modal.demo.ts
@@ -4,6 +4,7 @@
  * The full license information can be found in LICENSE in the root directory of this project.
  */
 import { Component } from '@angular/core';
+import '@clr/core/button';
 import '@clr/core/modal';
 
 @Component({
