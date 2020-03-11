d3b6b4ee940ff25a0c5b6212b52446021a3e2a30
Cory Rylan <crylan@vmware.com>
2020-03-11 15:25:19 -0500

    fix: fixup home page docs
    Signed-off-by: Cory Rylan <crylan@vmware.com>

--- src/clr-core/docs/welcome.stories.ts
+++ src/clr-core/docs/welcome.stories.ts
@@ -21,62 +21,45 @@ export default {
 export const core = () => {
   return html`
     <style> 
-      .welcome {
-        margin: 0 auto;
-        max-width: 34rem;
-        text-align: center;
-        padding-top: 11vh;
+      .welcome-intro {
+        max-width: 550px;
       }
 
-      .welcome h1 {
-        font-size: 3rem;
-        font-weight: 200;
-        margin: 0 0 1.3rem 0;
-      }
-
-      .welcome h2 {
-        font-size: 1rem;
-        margin: 0 0 1.4rem 0;
-      }
-
-      .welcome img {
+      img {
         width: 100%;
         max-width: 5rem;
-        margin: 0 auto 1rem auto;
-        display: block;
       }
 
-      .welcome p {
-        text-align: left;
-        line-height: 1.1rem;
-        max-width: 550px;
-        margin: 0 auto 0.9rem auto;
+      section {
+        padding-top: 11vh;
       }
     </style>
-    <section class="welcome">
-      <img src="./assets/images/clarity-logo.svg" alt="Clarity Core" />
-      <h1>Clarity Core <clr-icon shape="beta" size="xl" solid status="info" style="transform: translate3d(-0.3em, -0.4em, 0)"></clr-icon></h1>
-      <h2>Web Component Implementation of the Clarity Design System</h2>
+    <section cds-layout="vertical gap-lg align-items-center" cds-text="center">
+      <img src="./assets/images/clarity-logo.svg" cds-layout="align-stretch" alt="Clarity Core" />
+      <h1 cds-text="display">Clarity Core <clr-icon shape="beta" size="xl" solid status="info" style="transform: translate3d(-0.3em, -0.4em, 0)"></clr-icon></h1>
+      <h2 cds-text="section">Web Component Implementation of the Clarity Design System</h2>
 
-      <p>
-        Clarity Core is a Web Component implementation of the <a href="https://clarity.design">Clarity Design System</a>.
-        Clarity Core provides a set of reusable UI components that work in any JavaScript framework or no framework at all.
-      </p>
+      <div cds-layout="vertical gap-sm" cds-text="body left" class="welcome-intro">
+        <p>
+          Clarity Core is a Web Component implementation of the <a href="https://clarity.design">Clarity Design System</a>.
+          Clarity Core provides a set of reusable UI components that work in any JavaScript framework or no framework at all.
+        </p>
 
-      <p>
-        Core is currently in a beta state but is ready to try out.
-        We are actively looking for <a href="https://github.com/vmware/clarity/issues/new/choose">feedback</a>
-        from teams on their experiences. You can read more about Core and its goals
-        in our <a href="https://medium.com/claritydesignsystem/clarity-core-72f6d3a029bc" target="_blank" rel="noopener">Medium blog post</a>.
-      </p>
-      <br />
-
-      <cds-button status="primary">
-        <a href="./?path=/docs/documentation-getting-started--page">Get Started</a>
-      </cds-button>
-      <cds-button action="outline">
-        <a href="https://medium.com/claritydesignsystem/clarity-core-72f6d3a029bc" target="_blank" rel="noopener">Learn More</a>
-      </cds-button>
+        <p>
+          Core is currently in a beta state but is ready to try out.
+          We are actively looking for <a href="https://github.com/vmware/clarity/issues/new/choose">feedback</a>
+          from teams on their experiences. You can read more about Core and its goals
+          in our <a href="https://medium.com/claritydesignsystem/clarity-core-72f6d3a029bc" target="_blank" rel="noopener">Medium blog post</a>.
+        </p>
+      </div>
+      <div cds-layout="horizontal gap-xs align-items-center">
+        <cds-button status="primary">
+          <a href="./?path=/docs/documentation-getting-started--page">Get Started</a>
+        </cds-button>
+        <cds-button action="outline">
+          <a href="https://medium.com/claritydesignsystem/clarity-core-72f6d3a029bc" target="_blank" rel="noopener">Learn More</a>
+        </cds-button>
+      </div>
     </section>
   `;
 };
